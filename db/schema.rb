282082ed9ff71a2000434a01a91fdf99ba176e5c
Justin Daniel <justinddaniel@gmail.com>
2017-11-14 20:37:53 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140310213347) do
+ActiveRecord::Schema.define(version: 20171114203735) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "content"
   end
 
 end
