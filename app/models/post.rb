8555504df46e12d6c9e97281c982db9248c26d80
Justin Daniel <justinddaniel@gmail.com>
2017-11-14 20:51:51 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
-  belongs_to :user 
-  has_many :tags 
+  belongs_to :user
+  has_many :tags, through: :tagpost
   validates :name, presence: true
   validates :content, presence: true
 end
