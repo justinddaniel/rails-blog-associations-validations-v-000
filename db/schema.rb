98c5cd4682ac6a40e696e933ed069ed1d64e0986
Justin Daniel <justinddaniel@gmail.com>
2017-11-14 20:44:40 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114204218) do
+ActiveRecord::Schema.define(version: 20171114204429) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "content"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
